123a6ce9e828178f8b1d12af74b95dd5d6df5c0e
Olga Chernega <chernega.o@gmail.com>
2022-08-01 18:51:05 +0300

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const arr = [];
+  let index = 0;
+  for (let i = 0; i < str.length; i++){
+    for (let [key, value] of bracketsConfig){
+      if (str[i] === value) {
+        if (arr[index - 1] === key) {
+          arr.pop();
+          index--; 
+        }else {
+          arr.push(str[i]);
+          index++;
+        }
+      }else {
+        if (str[i] === key){
+          arr.push(str[i]);
+          index++;
+        }        
+      }
+    }
+  }
+  return (arr.length === 0);
 }
