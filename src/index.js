02e9c1c0628102f46fccec502b96533c07514414
Olga Chernega <chernega.o@gmail.com>
2022-08-02 11:55:16 +0300

    refactor: beautify

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
   const arr = [];
   let index = 0;
-  for (let i = 0; i < str.length; i++){
-    for (let [key, value] of bracketsConfig){
+  for (let i = 0; i < str.length; i++) {
+    for (let [key, value] of bracketsConfig) {
       if (str[i] === value) {
         if (arr[index - 1] === key) {
           arr.pop();
           index--; 
-        }else {
+        }
+        else {
           arr.push(str[i]);
           index++;
         }
-      }else {
-        if (str[i] === key){
+      }
+      else {
+        if (str[i] === key) {
           arr.push(str[i]);
           index++;
         }        
